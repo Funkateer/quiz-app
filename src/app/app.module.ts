e9d4df997ee372aee6954c332d313bb25cd1b009
alà eddin abugrara <alaeddin.abugrara@gmail.com>
2019-07-01 18:31:56 +0200

    generate reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { QuestionFormComponent } from './question-form/question-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionFormComponent
   ],
   imports: [
     BrowserModule,
