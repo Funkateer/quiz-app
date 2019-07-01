77b0720639eadfb17441925edcb50f4e1c1f68e7
alà eddin abugrara <alaeddin.abugrara@gmail.com>
2019-07-01 17:21:22 +0200

    use injectable decorator to make services available in root

--- src/app/questions.service.ts
+++ src/app/questions.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuestionsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  public getJSON(fileId: string) {
+    return this.http.get(`./assets/${fileId}.json`) as Observable<any>;
+  }
 }
