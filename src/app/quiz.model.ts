c2d5539dd6feb13a8cf33040773a30cba47abb47
alà eddin abugrara <alaeddin.abugrara@gmail.com>
2019-07-02 19:42:31 +0200

    add another quiz

--- src/app/quiz.model.ts
+++ src/app/quiz.model.ts
@@ -7,7 +7,7 @@ export class Question {
 }
 
 export class Quiz {
-  constructor(public label: string, public questions: Question[]) {}
+  constructor(public label: string, public name: string, public description: string, public fileName: string) {}
 }
 
 export class Answers {
