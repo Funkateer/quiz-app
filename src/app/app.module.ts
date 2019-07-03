fcda077c35cc6008757655fdb1856d506feede09
alà eddin abugrara <alaeddin.abugrara@gmail.com>
2019-07-03 16:13:55 +0200

    add routings for welcome page where user can choose from multiple quizzes, add new 'questions' component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { QuestionFormComponent } from './question-form/question-form.component';
 import { ResultsComponent } from './results/results.component';
+import { QuestionsComponent } from './questions/questions.component';
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: ':quizId', component: QuestionsComponent },
+  { path: '', redirectTo: "welcome", pathMatch: "prefix" },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     QuestionFormComponent,
-    ResultsComponent
+    ResultsComponent,
+    QuestionsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
